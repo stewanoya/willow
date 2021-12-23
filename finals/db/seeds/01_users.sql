9de9c34f1186fd2e06796092e51f75bf3d6d36e2
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-12-23 17:06:23 -0500

    Update users seeds

--- finals/db/seeds/01_users.sql
+++ finals/db/seeds/01_users.sql
@@ -1,10 +1,20 @@
 -- Users seeds
-INSERT INTO users (email, password, organization_name, phone, therapist) 
+INSERT INTO users (email, password, organization_name, is_therapist) 
 VALUES 
-('alice12@gmail.com', 'password123', Sunnyside Elementary School, 437-555-2353, Jordan Smith),
-('meow2021@gmail.com', 'pawsword123', Riverside Elementary School, 647-555-2376, Lina Ali),
-('md23@gmail.com', 'password123', Spring Gardens School, 416-555-7698, Anthony Jones),
-('julia123@gmail.com', 'pawsword123', Elk Creek Middle School, 905-555-3235, Shaun Chi),
-('alice12@gmail.com', 'password123', Waterford Heights, 905-555-1378, Eve Rogers);
+('alice12@gmail.com', 'password123', "Sunnyside Elementary School", false),
+('meow2021@gmail.com', 'pawsword123', "Riverside Elementary School", false),
+('md23@gmail.com', 'password123', "Spring Gardens School", false),
+('julia123@gmail.com', 'pawsword123', "Elk Creek Middle School", false),
+('tina12@gmail.com', 'password123', "Waterford Heights", false);
+('therapist1@gmail.com', 'password123', "Waterford Heights", true);
+
+
+
+
+
+
+
+
+
 
 
