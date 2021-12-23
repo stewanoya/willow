7555207f47d394fe05ef0a573120709af0d69b45
Stew Anoya <stewartanoya@gmail.com>
2021-12-23 16:37:25 -0500

    create habits seedS

---
+++ finals/db/seeds/04_habits.sql
@@ -0,0 +1,14 @@
+INSERT INTO habits (habit, student_id, habit_image_id)
+VALUES
+("Walk Dog", 1, "https://img.icons8.com/ios/48/000000/sun--v1.png"),
+("Workout", 2, "https://img.icons8.com/material-outlined/48/000000/dumbbell.png"),
+("Drink Water", 3, "https://img.icons8.com/material-outlined/48/000000/water.png"),
+("Save Money", 1, "https://img.icons8.com/material-outlined/48/000000/money--v1.png"),
+("Save Money", 2, "https://img.icons8.com/material-outlined/48/000000/money--v1.png"),
+("Save Money", 3, "https://img.icons8.com/material-outlined/48/000000/money--v1.png"),
+("Eat Veggies", 1 "https://img.icons8.com/windows/48/000000/broccoli.png"),
+("Eat Veggies", 2 "https://img.icons8.com/windows/48/000000/broccoli.png"),
+("Study", 4, "https://img.icons8.com/material-outlined/48/000000/brain--v1.png"),
+("Clean", 5, "https://img.icons8.com/external-konkapp-detailed-outline-konkapp/48/000000/external-sweeping-hygiene-routine-konkapp-detailed-outline-konkapp.png"),
+("Clean", 4, "https://img.icons8.com/external-konkapp-detailed-outline-konkapp/48/000000/external-sweeping-hygiene-routine-konkapp-detailed-outline-konkapp.png"),
+("Do Homework", 5, "https://img.icons8.com/material-outlined/24/000000/pencil--v1.png")
