05a48f1a71f8e3874c351cb89f1fda58f7687ccd
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-12-23 16:38:52 -0500

    Add users and journals

--- finals/db/seeds/01_users.sql
+++ finals/db/seeds/01_users.sql
@@ -5,6 +5,6 @@ VALUES
 ('meow2021@gmail.com', 'pawsword123', Riverside Elementary School, 647-555-2376, Lina Ali),
 ('md23@gmail.com', 'password123', Spring Gardens School, 416-555-7698, Anthony Jones),
 ('julia123@gmail.com', 'pawsword123', Elk Creek Middle School, 905-555-3235, Shaun Chi),
-('alice12@gmail.com', 'password123', Waterford Heights, 905-555-1378, Eve Rogers);
+('alice112@gmail.com', 'password123', Waterford Heights, 905-555-1378, Eve Rogers);
 
 
