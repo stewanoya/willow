d12434aaead8550bfff0d0a69fe9f0709c987b3d
Muna Dahir <muna.dahir@hotmail.com>
2021-12-23 17:35:49 -0500

    single quote

--- finals/db/seeds/01_users.sql
+++ finals/db/seeds/01_users.sql
@@ -1,12 +1,12 @@
 -- Users seeds
 INSERT INTO users (email, password, organization_name, is_therapist) 
 VALUES 
-('alice12@gmail.com', 'password123', "Sunnyside Elementary School", false),
-('meow2021@gmail.com', 'pawsword123', "Riverside Elementary School", false),
-('md23@gmail.com', 'password123', "Spring Gardens School", false),
-('julia123@gmail.com', 'pawsword123', "Elk Creek Middle School", false),
-('tina12@gmail.com', 'password123', "Waterford Heights", false);
-('therapist1@gmail.com', 'password123', "Waterford Heights", true);
+('alice12@gmail.com', 'password123', 'Sunnyside Elementary School', false),
+('meow2021@gmail.com', 'pawsword123', 'Riverside Elementary School', false),
+('md23@gmail.com', 'password123', 'Spring Gardens School', false),
+('julia123@gmail.com', 'pawsword123', 'Elk Creek Middle School', false),
+('tina12@gmail.com', 'password123', 'Waterford Heights', false),
+('therapist1@gmail.com', 'password123', 'Waterford Heights', true);
 
 
 
