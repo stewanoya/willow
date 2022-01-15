ecda0d6a4020b197bf081940f1d436a35abb0402
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-15 17:15:57 -0500

    Removed subtitle

--- client/src/components/calm-corner/CarouselData.js
+++ client/src/components/calm-corner/CarouselData.js
@@ -5,17 +5,14 @@ import image3 from "./carousel-images/image3.jpeg";
 export const images = [
   {
     affirmation: "My failures do not define me, they are redirections.",
-    subtitle: "This is picture 1",
     img: image1,
   },
   {
     affirmation: "I am in the right place, at the right time, doing the right thing.",
-    subtitle: "This is picture 2",
     img: image2,
   },
   {
     affirmation: "You are worthy for being who you are, just for existing.",
-    subtitle: "This is picture 3",
     img: image3
   },
 ];
