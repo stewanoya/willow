6fcd68cbfc42fa1d3ffda5e07fbdb51d0f347d72
Stew Anoya <stewartanoya@gmail.com>
2022-01-15 09:17:03 -0500

    Create basic form with state

--- client/src/components/AuthForm.jsx
+++ client/src/components/AuthForm.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import axios from "axios";
+
 const AuthForm = () => {
-  return <h1>Hello</h1>;
+  const [user, setUser] = useState({});
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  const emailHandler = (e) => {
+    setUser((prev) => {
+      // I used user here instead of email because I think that's the key
+      // basic auth is expecting? Not sure though.
+      return { ...prev, user: e.target.value };
+    });
+  };
+
+  const passHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, password: e.target.value };
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input placeholder="email" onChange={emailHandler} type="email" />
+      <input placeholder="password" onChange={passHandler} type="password" />
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default AuthForm;
