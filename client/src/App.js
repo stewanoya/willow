8b930bc8096b9052bec44856beeb1ad2cf012553
Stew Anoya <stewartanoya@gmail.com>
2022-01-15 15:00:23 -0500

    Add css reset

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/user" element={<AuthForm />} />
           <Route path="/main" element={<Main />} />
         </Routes>
