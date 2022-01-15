85c898381d63bad981a45e1fe044b4aab2d4020f
Stew Anoya <stewartanoya@gmail.com>
2022-01-15 15:22:32 -0500

    Add state to sidebar for views

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import Sidebar from "./Sidebar/Sidebar";
 
 const Main = () => {
+  const [view, setView] = useState("home");
+
   const user = localStorage.getItem("user");
+
+  const getView = (view) => {
+    setView(view);
+  };
   return (
     <div>
       <h1>Main App</h1>
-      <Sidebar />
+      <Sidebar getView={getView} />
     </div>
   );
 };
