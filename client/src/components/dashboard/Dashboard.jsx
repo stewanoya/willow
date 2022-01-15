9d243b3396b2722cbef1b68152c4a1e6324d4693
Stew Anoya <stewartanoya@gmail.com>
2022-01-15 15:56:47 -0500

    Create dashboard component

---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <Header />
+    </div>
+  );
+};
+
+export default Dashboard;
