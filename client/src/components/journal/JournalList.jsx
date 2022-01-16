7b086e1e4c72dcc50262be79929883820977e19e
Muna Dahir <muna.dahir@hotmail.com>
2022-01-16 00:29:02 -0500

    display the journal entries using state

--- client/src/components/journal/JournalList.jsx
+++ client/src/components/journal/JournalList.jsx
@@ -1,21 +1,45 @@
 import JournalListItem from "./JournalListItem";
+// import Journalhow from "./JournalShow";
+import "./Journal.css";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const JournalList = () => {
-  const [journal, setJournal] = useState([]);
-  const [selectedJournal, setSelectedJournal] = useState(null);
-
+  const [journals, setJournals] = useState([]);
+  console.log(journals);
   useEffect(() => {
     let email = localStorage.getItem("user");
     axios.get(`http://localhost:3002/journals/user/${email}`).then((result) => {
-      // setJournal(result.data);
-      console.log(result);
+      setJournals(result.data);
     });
   }, []);
 
-  return <div className=''> Title </div>;
+  // const getJournal = (id) => {
+  //   const filteredJournal = journals.filter((entry) => entry.id === id);
+  //   setSelectedJournal(filteredJournal);
+  // };
+
+  const parsedJournals = journals.map((journal) => {
+    return (
+      <div className='card'>
+        <JournalListItem
+          key={journal.id}
+          id={journal.id}
+          title={journal.title}
+          entry={journal.entry}
+          date={journal.date}
+          scale={journal.scale}
+        />
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div>{parsedJournals}</div>
+    </>
+  );
 };
 
 export default JournalList;
