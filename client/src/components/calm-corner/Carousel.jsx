5650a4e37c46078ec735e03be3e68f800ac62b0c
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-15 22:47:36 -0500

    Bug with initial render

--- client/src/components/calm-corner/Carousel.jsx
+++ client/src/components/calm-corner/Carousel.jsx
@@ -1,47 +1,45 @@
 import React, { useState, useEffect } from "react";
 import "./Carousel.css";
-import { images } from "./CarouselData";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import axios from "axios";
 
-
 function Carousel() {
   const URL = "http://localhost:3002/calm-images";
-  const [currentIMG, setCurrentIMG] = useState(0);
-  const [data, setData] = useState(images);
-  
-  
   useEffect(() => {
-    axios
-    .get(URL).then((response) => {
-      // setCurrentIMG(response.data)
+    axios.get(URL).then((response) => {
       setData(response.data.rows);
-    })
-  }, [])
+      console.log(response.data.rows);
+    });
+  }, []);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [data, setData] = useState(null);
 
   return (
     <div className="carousel">
       <div
         className="carouselInner"
-        style={{ backgroundImage: `url(${data[currentIMG].img})` }}
+        style={{ backgroundImage: `url(${data[currentIndex].img})` }}
       >
         <div
           className="left"
           onClick={() => {
-            currentIMG > 0 && setCurrentIMG(currentIMG - 1);
+            currentIndex > 0 && setCurrentIndex(currentIndex - 1);
           }}
         >
-          <ArrowBackIosIcon style={{fontSize: 30}}/>
+          <ArrowBackIosIcon style={{ fontSize: 30 }} />
         </div>
         <div className="center">
-          {/* <h1>{images[currentIMG].affirmation}</h1> */}
+          <h1>{data[currentIndex].affirmations}</h1>
         </div>
-
-        <div className="right"  onClick={() => {
-            currentIMG < data.length -1 && setCurrentIMG(currentIMG - 1);
-          }}>
-          <ArrowForwardIosIcon style={{fontSize: 30}} />
+        <div
+          className="right"
+          onClick={() => {
+            currentIndex < data.length - 1 && setCurrentIndex(currentIndex + 1);
+          }}
+        >
+          <ArrowForwardIosIcon style={{ fontSize: 30 }} />
         </div>
       </div>
     </div>
