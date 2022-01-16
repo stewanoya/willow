15d9f0cc207be75f094e23ee9ebed34d32fa05aa
Muna Dahir <muna.dahir@hotmail.com>
2022-01-16 16:57:46 -0500

    remove console.log

--- client/src/components/Journal/Journal.jsx
+++ client/src/components/Journal/Journal.jsx
@@ -2,7 +2,6 @@ import JournalList from "./JournalList";
 import Header from "./Header";
 
 const JournalListItem = (props) => {
-  console.log(props);
   return (
     <div className='main-journal-container' m>
       <Header />
