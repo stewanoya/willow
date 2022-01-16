c690cf0f88247e929463961b2f5dde515c854852
Stew Anoya <stewartanoya@gmail.com>
2022-01-16 12:02:55 -0500

    fix merge conflict

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -16,15 +16,8 @@ const Main = () => {
     setView(view);
   };
   return (
-<<<<<<< HEAD
-    <div
-      className="app-container"
-      style={{ "background-color": "#E0ECF8", height: "100vh" }}
-    >
-=======
     <div className="app-container">
       <h1>Main App</h1>
->>>>>>> 5fcd24c63774574d969d2fb383ec11fd6be211e0
       {view === "home" && <Dashboard />}
       {view === "calm" && <CalmCorner />}
       {view === "journal" && <Journal />}
