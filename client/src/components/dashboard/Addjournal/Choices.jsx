994db8992a9bea50a3625b20ee68d083f2cd8973
Stew Anoya <stewartanoya@gmail.com>
2022-01-16 09:44:09 -0500

    create journal choices state and component

---
+++ client/src/components/dashboard/Addjournal/Choices.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+import { useEffect } from "react";
+
+const Choices = (props) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Choices;
