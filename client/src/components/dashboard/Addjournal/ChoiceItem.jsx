2f6c53b81d1150b42fc82e397a08842dd7b95821
Stew Anoya <stewartanoya@gmail.com>
2022-01-16 10:44:14 -0500

    style choices and collect choice data in addjournal

---
+++ client/src/components/dashboard/Addjournal/ChoiceItem.jsx
@@ -0,0 +1,17 @@
+const ChoiceItem = (props) => {
+  const { image, name, id, getChoice, getChoiceMade } = props;
+
+  const clickHandler = () => {
+    getChoice(name);
+    getChoiceMade();
+  };
+
+  return (
+    <div className="choice-item" onClick={clickHandler}>
+      <p className="choice-name">{name}</p>
+      <img src={image} className="choice-image" />
+    </div>
+  );
+};
+
+export default ChoiceItem;
