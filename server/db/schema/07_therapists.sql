ee26c766abee28f6c3a7edc375c758289206f19e
Muna Dahir <muna.dahir@hotmail.com>
2022-01-17 01:25:12 -0500

    edit therapist schema and seeds

--- server/db/schema/07_therapists.sql
+++ server/db/schema/07_therapists.sql
@@ -6,6 +6,8 @@ DROP TABLE IF EXISTS therapists CASCADE;
 CREATE TABLE therapists (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  img TEXT NOT NULL,
   phone VARCHAR(15) NOT NULL,
   organization_name VARCHAR(255)
 );
--- server/db/seeds/07_therapists.sql
+++ server/db/seeds/07_therapists.sql
@@ -1,8 +1,8 @@
-INSERT INTO therapists (name, phone, organization_name)
+INSERT INTO therapists (name, email, img, phone, organization_name)
 VALUES 
-('Fatimah Ali', 647-555-3464, 'Family Medical Team'),
-('Anthony Richard', 416-555-6325, 'Grassroots Therapy'),
-('Sung Hoon', 416-555-2345, 'Rubber Ducky Group'),
-('Eva Martinez', 416-555-1958, 'OneHealth Therapy'),
-('Sarah Jones', 416-555-5928, 'TwoHealth Therapy');
+('Fatimah Ali', 'fatimah_ali@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '647-555-3464', 'Family Medical Team'),
+('Anthony Richard', 'anthony_Richard@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '416-555-6325', 'Grassroots Therapy'),
+('Sung Hoon', 'sung_hoon@gmail.com', '416-555-2345', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', 'Rubber Ducky Group'),
+('Eva Martinez', 'eva_martinez@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '416-555-1958', 'OneHealth Therapy'),
+('Sarah Jones', 'sarah_jones@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '416-555-5928', 'TwoHealth Therapy');
 
