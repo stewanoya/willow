cbb4917e4865323fb1d8f5caeefa557ec739ccbe
Stew Anoya <stewartanoya@gmail.com>
2022-01-16 19:17:58 -0500

    add state to text areas and form

--- client/src/components/dashboard/Addjournal/JournalText.jsx
+++ client/src/components/dashboard/Addjournal/JournalText.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+
 const JournalText = (props) => {
+  const [text, setText] = useState({ title: "", textArea: "" });
+
+  const changeTitleHandler = (e) => {
+    setText((prev) => {
+      return { ...prev, title: e.target.value };
+    });
+  };
+
+  const changeTextAreaHandler = (e) => {
+    setText((prev) => {
+      return { ...prev, textArea: e.target.value };
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h3 className="choice-title">What happened?</h3>
-      <form className="journal-form">
+      <form className="journal-form" onSubmit={submitHandler}>
         <label>
           <input
             name="title"
             placeholder="Title.."
             className="journal-text-title"
+            value={text.title}
+            onChange={changeTitleHandler}
           />
         </label>
         <label>
@@ -15,8 +37,15 @@ const JournalText = (props) => {
             name="text"
             placeholder="Let it out."
             className="journal-text"
+            value={text.textArea}
+            onChange={changeTextAreaHandler}
           />
         </label>
+        {text.textArea.length > 0 && (
+          <button className="journal-submit-button" type="submit">
+            Save
+          </button>
+        )}
       </form>
     </>
   );
