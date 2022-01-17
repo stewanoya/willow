76e1148459f8196c12fff89d392f65735b5bd040
Stew Anoya <stewartanoya@gmail.com>
2022-01-16 21:29:35 -0500

    add conditional styyling using classnames

--- client/src/components/dashboard/Addjournal/ChoiceItem.jsx
+++ client/src/components/dashboard/Addjournal/ChoiceItem.jsx
@@ -1,17 +1,23 @@
 import classNames from "classnames";
+import { useState } from "react";
 
 const ChoiceItem = (props) => {
   const { image, name, getChoice, getChoiceMade } = props;
 
-  const choiceClass = classNames();
+  const [click, setClick] = useState(false);
+
+  const choiceClass = classNames("choice-item", {
+    "choice-item-selected": click,
+  });
 
   const clickHandler = () => {
     getChoice(name);
     getChoiceMade();
+    setClick(true);
   };
 
   return (
-    <div className="choice-item" onClick={clickHandler}>
+    <div className={choiceClass} onClick={clickHandler}>
       <p className="choice-name">{name}</p>
       <img src={image} className="choice-image" alt="choices" />
     </div>
