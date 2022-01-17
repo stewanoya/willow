2c6db0a4169fd56fff71b1a02d1ff06bf1c72dd1
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-17 15:54:43 -0500

    Pause and play working on one button click

--- client/src/components/calm-corner/Breathing.jsx
+++ client/src/components/calm-corner/Breathing.jsx
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
-import classNames from 'classnames';
+import classNames from "classnames";
 import "./Breathing.css";
 
-
 function Breathing() {
-  const [ animation, setAnimation ] = useState(false)
-  const breatheClass = classNames("unanimated", {"breathe": animation})
+  const [animation, setAnimation] = useState(false);
+  const breatheClass = classNames("unanimated", { breathe: animation });
+
+  const handlePlay = () => {
+    if (animation === true){
+      setAnimation(false)
+    } else {
+      setAnimation(true)
+    }
+  };
 
   return (
     <>
-        <div id="breathe-card">
-          <div className="instructions">
-            <span>Breathing Exercise </span>
-            <p>Inhale slowly through the nose as the circle expands,</p>
-            <p> and breathe out through the mouth as it contracts. </p>
-            <p>Repeat as much as you need, to feel calm.</p>
-          </div>
-          <div className="breathe-container">
-            <div className={breatheClass}></div>
-          </div>
-          <button className="button" onClick={() => {setAnimation(true)}}>Play</button>
-          <button className="button" onClick={() => {setAnimation(false)}}>Pause</button>
+      <div id="breathe-card">
+        <div className="instructions">
+          <span>Breathing Exercise </span>
+          <p>Inhale slowly through the nose as the circle expands,</p>
+          <p> and breathe out through the mouth as it contracts. </p>
+          <p>Repeat as much as you need, to feel calm.</p>
         </div>
+        <div className="breathe-container">
+          <div className={breatheClass}></div>
+        </div>
+        <div className="button-container">
+          <button className="button-play" onClick={handlePlay}>Pause</button>
+        </div>
+      </div>
     </>
   );
 }
