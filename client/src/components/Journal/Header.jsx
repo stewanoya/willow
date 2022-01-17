c72d4474f3ebd7d270cad39b423caa21b817496f
Stew Anoya <stewartanoya@gmail.com>
2022-01-16 21:52:33 -0500

    undo changes"

--- client/src/components/Journal/Header.jsx
+++ client/src/components/Journal/Header.jsx
@@ -1,7 +1,7 @@
 //should hold the add journal button component
 
 const Header = () => {
-  return <div className="journal-header">Add Journal entry</div>;
+  return <div className="header">Add Journal entry</div>;
 };
 
 export default Header;
