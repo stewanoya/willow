ba05a88c07de25afc3e61b931c774ae3e9da595a
Stew Anoya <stewartanoya@gmail.com>
2022-01-17 08:48:19 -0500

    install mui dependencies

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -1,9 +1,18 @@
+import "./Home.css";
+import Button from "@mui/material/Button";
+
 import Nav from "./Nav";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <Nav />
+      <div className="hero">
+        <div className="get-started-container">
+          <h2 className="hero-title">Health & Wellness For Kids</h2>
+        </div>
+        <div className="happy-teen"></div>
+      </div>
     </div>
   );
 };
--- client/src/components/Homepage/Nav.jsx
+++ client/src/components/Homepage/Nav.jsx
@@ -1,5 +1,18 @@
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+
 const Nav = () => {
-  return <h1>hello</h1>;
+  return (
+    <nav className="navbar">
+      <h1 className="app-name">AppName</h1>
+      <div className="buttons-container">
+        <Stack spacing={3} direction="row">
+          <Button variant="contained">Login</Button>
+          <Button variant="outlined">Register</Button>
+        </Stack>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
