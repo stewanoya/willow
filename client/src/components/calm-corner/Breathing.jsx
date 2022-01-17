c1130719122b665da023f86535f888e076b504da
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-17 12:37:11 -0500

    Fix bug

--- client/src/components/calm-corner/Breathing.jsx
+++ client/src/components/calm-corner/Breathing.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from 'classnames';
 import "./Breathing.css";
-import Card from "../UI/Card";
+
 
 function Breathing() {
+  const [ animation, setAnimation ] = useState(false)
+  const breatheClass = classNames("unanimated", {"breathe": animation})
+
+  const playButton = () => {
+    if(setAnimation(true)){
+      setAnimation(false)
+    }
+  }
   return (
     <>
-      <Card className="breathe-card">
-        <div className="instructions">
-          <span>Breathing Exercise </span>
-          <p>Inhale slowly through the nose as the circle expands,</p>
-          <p> and breathe out through the mouth as it contracts. </p>
-          <p>Repeat as much as you need, to feel calm.</p>
+        <div id="breathe-card">
+          <div className="instructions">
+            <span>Breathing Exercise </span>
+            <p>Inhale slowly through the nose as the circle expands,</p>
+            <p> and breathe out through the mouth as it contracts. </p>
+            <p>Repeat as much as you need, to feel calm.</p>
+          </div>
+          <div className="breathe-container">
+            <div className={breatheClass}></div>
+          </div>
+          <button className="button" onClick={() => {setAnimation(true)}}>Play</button>
+          <button className="button" onClick={() => {setAnimation(false)}}>Pause</button>
         </div>
-        <div className="breathe-container">
-          <div className="breathe"></div>
-        </div>
-      </Card>
     </>
   );
 }
