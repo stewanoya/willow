f475c9815097fc948cdb60ae0ebc380533fa185c
Stew Anoya <stewartanoya@gmail.com>
2022-01-17 11:00:18 -0500

    add hero section with styling

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -1,21 +1,18 @@
 import "./Home.css";
-import Theme from "../ThemeMUI/Theme";
+
 import { ThemeProvider } from "@mui/material/styles";
 
+import Theme from "../ThemeMUI/Theme";
 import Nav from "./Nav";
+import Hero from "./Hero";
 
 const Home = () => {
   return (
     <div className="home-container">
       <ThemeProvider theme={Theme}>
         <Nav />
+        <Hero />
       </ThemeProvider>
-      <div className="hero">
-        <div className="get-started-container">
-          <h2 className="hero-title">Health & Wellness For Kids</h2>
-        </div>
-        <div className="happy-teen"></div>
-      </div>
     </div>
   );
 };
