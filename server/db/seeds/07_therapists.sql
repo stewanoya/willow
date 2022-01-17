e8a057ff07658e1690dd3467598821c9fe227995
Muna Dahir <muna.dahir@hotmail.com>
2022-01-17 01:32:40 -0500

    fix typo in seed file

--- server/db/seeds/07_therapists.sql
+++ server/db/seeds/07_therapists.sql
@@ -1,8 +1,8 @@
 INSERT INTO therapists (name, email, img, phone, organization_name)
 VALUES 
-('Fatimah Ali', 'fatimah_ali@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '647-555-3464', 'Family Medical Team'),
+('Fatimah Ali', 'fatimah_ali@gmail.com', 'https://static.vecteezy.com/system/resources/previews/002/318/271/non_2x/user-profile-icon-free-vector.jpg', '647-555-3464', 'Family Medical Team'),
 ('Anthony Richard', 'anthony_Richard@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '416-555-6325', 'Grassroots Therapy'),
-('Sung Hoon', 'sung_hoon@gmail.com', '416-555-2345', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', 'Rubber Ducky Group'),
+('Sung Hoon', 'sung_hoon@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '416-555-2345','Rubber Ducky Group'),
 ('Eva Martinez', 'eva_martinez@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '416-555-1958', 'OneHealth Therapy'),
 ('Sarah Jones', 'sarah_jones@gmail.com', 'https://media.istockphoto.com/vectors/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-vector-id1270368615?k=20&m=1270368615&s=170667a&w=0&h=qpvA8Z6L164ZcKfIyOl-E8fKnfmRZ09Tks7WEoiLawA=', '416-555-5928', 'TwoHealth Therapy');
 
