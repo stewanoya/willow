4d094c36fa7f451ea7631f71641fa2b86c3d75a7
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-17 20:35:04 -0500

    Add scroll and update CSS in Calm Corner, etc

--- client/src/components/CalmCorner/Breathing.jsx
+++ client/src/components/CalmCorner/Breathing.jsx
@@ -18,29 +18,30 @@ function Breathing() {
 
   return (
     <>
-      <div id="breathe-card">
-        <div className="instructions">
-          <span>Breathing Exercise </span>
-          <p>Inhale slowly through the nose as the circle expands,</p>
-          <p> and breathe out through the mouth as it contracts. </p>
-          <p>Repeat as much as you need, to feel calm.</p>
-        </div>
-        <div className="breathe-container">
-          <div className={breatheClass}></div>
-        </div>
+        <div id="breathe-card">
+          <div className="instructions">
+            <span>Breathing Exercise </span>
+            <p>Inhale slowly through the nose as the circle expands,</p>
+            <p> and breathe out through the mouth as it contracts. </p>
+            <p>Repeat as much as you need, to feel calm.</p>
+          </div>
+          <div className="breathe-container">
+            <div className={breatheClass}></div>
+          </div>
 
-        <div className="button-container">
-          {animation === true ? (
-            <button className="button play" onClick={handlePlay}>
-              <PauseIcon />
-            </button>
-          ) : (
-            <button className="button pause" onClick={handlePlay}>
-              <PlayArrowIcon />
-            </button>
-          )}
+          <div className="button-container">
+            {animation === true ? (
+              <button className="button play" onClick={handlePlay}>
+                <PauseIcon />
+              </button>
+            ) : (
+              <button className="button pause" onClick={handlePlay}>
+                <PlayArrowIcon />
+              </button>
+            )}
+          </div>
         </div>
-      </div>
+  
     </>
   );
 }
