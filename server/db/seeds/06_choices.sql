5e76938af1f6d194085cc8f39d402cec577e3c9e
Stew Anoya <stewartanoya@gmail.com>
2022-01-18 11:50:31 -0500

    change seeds to use local resources

--- server/db/seeds/06_choices.sql
+++ server/db/seeds/06_choices.sql
@@ -1,11 +1,11 @@
 INSERT INTO choices (label, image)
 VALUES
-('Family', 'https://cdn-icons-png.flaticon.com/512/845/845022.png'),
-('Friends', 'https://img.icons8.com/external-wanicon-lineal-wanicon/64/000000/external-friend-friendship-wanicon-lineal-wanicon.png'),
-('Socials', 'https://img.icons8.com/material-outlined/96/000000/facebook-new.png'),
-('Food', 'https://cdn-icons-png.flaticon.com/512/857/857681.png'),
-('Health', 'https://cdn-icons-png.flaticon.com/512/2966/2966334.png'),
-('Sports', 'https://cdn-icons-png.flaticon.com/512/857/857492.png');
+('Family', './icons/home.png'),
+('Friends', './icons/friends.png'),
+('Socials', './icons/social-media.png'),
+('Food', './icons/cutlery.png'),
+('Health', './icons/mental-health.png'),
+('Sports', './icons/balls-sports.png');
 
 
 
