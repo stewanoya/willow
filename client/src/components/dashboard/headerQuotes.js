a8c0a3c94b1dfb335fab58bcf11e3d980760ac67
Stew Anoya <stewartanoya@gmail.com>
2022-01-17 22:49:55 -0500

    create list of positive quotes

---
+++ client/src/components/dashboard/headerQuotes.js
@@ -0,0 +1,29 @@
+const headerQuotes = [
+  { quote: "Love For All, Hatred For None.", author: "Khalifatul Masih III" },
+  { quote: "Change the world by being yourself.", author: "Amy Poehler" },
+  { quote: "Every moment is a fresh beginning.", author: "T.S Eliot" },
+  { quote: "Never regret anything that made you smile.", author: "Mark Twain" },
+  { quote: "Everything you can imagine is real.", author: "Pablo Picasso" },
+  { quote: "Whatever you do, do it well.", author: "Walt Disney" },
+  {
+    quote: "Problems are not stop signs, they are guidelines.",
+    author: "Robert H. Schiuller",
+  },
+  {
+    quote: "Tough times never last but tough people do.",
+    author: "Robert H. Schiuller",
+  },
+  {
+    quote: "Hate comes from intimidation, love comes from appreciation.",
+    author: "Tyga",
+  },
+  { quote: "Be so good they can't ignore you.", author: "Steve Martin" },
+  { quote: "Try to be a rainbow in someone’s cloud.", author: "Maya Angelou" },
+  {
+    quote: "Wanting to be someone else is a waste of who you are.",
+    author: "Kurt Cobain",
+  },
+  { quote: "Let the beauty of what you love be what you do.", author: "Rumi" },
+];
+
+export default headerQuotes;
