e2e1059854eb1c97f38f1555d30eae96b6b8315f
Stew Anoya <stewartanoya@gmail.com>
2022-01-18 16:31:49 -0500

    reposition info bubbles for new size

--- client/src/components/Homepage/Home.jsx
+++ client/src/components/Homepage/Home.jsx
@@ -7,6 +7,8 @@ import Nav from "./Nav";
 import Hero from "./Hero";
 import SectionTop from "./SectionTop";
 import SectionMiddle from "./SectionMiddle";
+import SectionBottom from "./SectionBottom";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
@@ -16,6 +18,8 @@ const Home = () => {
         <Hero />
         <SectionTop />
         <SectionMiddle />
+        <SectionBottom />
+        <Footer />
       </ThemeProvider>
     </div>
   );
