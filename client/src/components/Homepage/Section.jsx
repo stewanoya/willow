40f8911237b91be1f87ac44d40febf925aaf8c66
Stew Anoya <stewartanoya@gmail.com>
2022-01-18 09:14:47 -0500

    reconfigure absolute positions for info bubbles

--- client/src/components/Homepage/Section.jsx
+++ client/src/components/Homepage/Section.jsx
@@ -3,17 +3,19 @@ const Section = () => {
     <section className="app-info-section">
       <h1 className="app-info-title">Everything You Need</h1>
       <div className="mockup-container">
-        <img
-          src={require("./assets/iphone-mockup.png")}
-          className="iphone-mockup"
-        />
-        <div className="info-bubbles-container">
+        <div className="iphone-mockup">
+          {" "}
+          <div className="info-bubbles-container"></div>
           <div className="info-bubble-right">
-            <p>Testing an info bubble</p>
+            <p>Dashboard with quick insights and insipiring words.</p>
           </div>
           <div className="spacer"></div>
           <div className="info-bubble-left">
-            <p>Testing another info bubble</p>
+            <p>Simple Navigation UI</p>
+          </div>
+          <div className="spacer"></div>
+          <div className="info-bubble-right">
+            <p>Emoji faces to discover and learn about emotions.</p>
           </div>
         </div>
       </div>
