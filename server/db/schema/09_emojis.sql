d976e8adedad65969183e42667605ab75713c1a6
Stew Anoya <stewartanoya@gmail.com>
2022-01-17 22:02:36 -0500

    Change emoji source to link

--- server/db/schema/09_emojis.sql
+++ server/db/schema/09_emojis.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS emojis CASCADE;
 CREATE TABLE emojis (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
-  icon VARCHAR(255),
+  icon TEXT,
   description TEXT,
   affirmation TEXT
 );
