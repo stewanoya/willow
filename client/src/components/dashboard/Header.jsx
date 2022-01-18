95354d6f4f1a93db56ca97df87688c9b9044b46b
Stew Anoya <stewartanoya@gmail.com>
2022-01-17 22:50:35 -0500

    use quotes in header

--- client/src/components/dashboard/Header.jsx
+++ client/src/components/dashboard/Header.jsx
@@ -1,11 +1,12 @@
 const Header = (props) => {
-  const { journalCount } = props;
+  const { journalCount, quote } = props;
+
   return (
     <div className="header-dashboard">
       <div className="dashboard-content">
-        <h3 className="header-affirmation">
-          Another beautiful day! Keep up the great work.
-        </h3>
+        <h3 className="header-affirmation">{quote.quote}</h3>
+        <p className="quote-author"> -{quote.author}</p>
+
         <p className="journal-count-title">
           Journal Entries:{" "}
           <strong className="journal-count">{journalCount}</strong>
