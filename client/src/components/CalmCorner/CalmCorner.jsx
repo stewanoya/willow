b904876a2599919f67f9a0a1cd27a03ea1886407
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2022-01-17 20:51:15 -0500

    Delete feature

--- client/src/components/CalmCorner/CalmCorner.jsx
+++ client/src/components/CalmCorner/CalmCorner.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Carousel from "./Carousel";
 import Breathing from "./Breathing"
 import "./CalmCorner.css";
+import CalmSounds from "./CalmSounds";
 
 function CalmCorner() {
   return (
     <div className="CalmCorner">
       <Carousel />
       <Breathing />
+      <CalmSounds />
     </div>
   );
 }
---
+++ client/src/components/CalmCorner/CalmSounds.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Card from '../UI/Card';
+
+function CalmSounds() {
+  return (
+    < Card >
+    </Card>
+  )
+}
+
+export default CalmSounds
