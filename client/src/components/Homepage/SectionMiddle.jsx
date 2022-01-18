4f90bde9fdc6ca32ef1ffcc3d0c4f37bdb2310b3
Stew Anoya <stewartanoya@gmail.com>
2022-01-18 10:15:46 -0500

    create new sections

---
+++ client/src/components/Homepage/SectionMiddle.jsx
@@ -0,0 +1,28 @@
+const SectionTop = () => {
+  return (
+    <>
+      <section className="app-info-section">
+        <h1 className="app-info-title">Everything You Need</h1>
+        <div className="mockup-container">
+          <div className="iphone-mockup">
+            {" "}
+            <div className="info-bubbles-container"></div>
+            <div className="info-bubble-right">
+              <p>Dashboard with quick insights and insipiring words.</p>
+            </div>
+            <div className="spacer"></div>
+            <div className="info-bubble-left">
+              <p>Simple Navigation UI</p>
+            </div>
+            <div className="spacer"></div>
+            <div className="info-bubble-right">
+              <p>Emoji faces to discover and learn about emotions.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SectionTop;
