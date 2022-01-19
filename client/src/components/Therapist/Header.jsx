6aef5a364b91d7ae6b4fb5ca12c75f81fc10e1b9
Muna Dahir <muna.dahir@hotmail.com>
2022-01-19 16:27:54 -0500

    add instructions to book

--- client/src/components/Therapist/Header.jsx
+++ client/src/components/Therapist/Header.jsx
@@ -1,7 +1,18 @@
 const Header = () => {
   return (
     <div className='therapist-header'>
-      Add instructions to book an interview
+      <article className='header-text'>
+        <h4>To book an appointment:</h4>
+        <p>
+          <span>1.</span> Choose the therpaist you'd like to book with.
+        </p>
+        <p>
+          <span>2.</span> Click the 'Book' button.
+        </p>
+        <p>
+          <span>3.</span> In the new window choose a day and time slot.
+        </p>
+      </article>
     </div>
   );
 };
