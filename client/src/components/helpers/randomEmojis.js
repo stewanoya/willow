fb1af0992af59016a9be41e64f101df33c2f242e
Stew Anoya <stewartanoya@gmail.com>
2022-01-19 12:53:18 -0500

    fix typo in function

--- client/src/components/helpers/randomEmojis.js
+++ client/src/components/helpers/randomEmojis.js
@@ -1,6 +1,6 @@
 const getRandomEmojis = (arr) => {
   const randomEmojis = [];
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 12; i++) {
     let randomIndex = Math.floor(Math.random() * arr.length);
 
     if (randomEmojis.includes(arr[randomIndex])) {
@@ -8,8 +8,10 @@ const getRandomEmojis = (arr) => {
     } else {
       randomEmojis.push(arr[randomIndex]);
     }
+    if (randomEmojis.length === 3) {
+      return randomEmojis;
+    }
   }
-  return randomEmojis;
 };
 
 export default getRandomEmojis;
