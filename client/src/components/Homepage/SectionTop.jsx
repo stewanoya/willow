1f71d3ec50482c51e66f27ef0ebb38adfdd5fb27
Stew Anoya <stewartanoya@gmail.com>
2022-01-20 10:32:31 -0500

    recolor homepage and reposition mockup bubbles

--- client/src/components/Homepage/SectionTop.jsx
+++ client/src/components/Homepage/SectionTop.jsx
@@ -4,19 +4,20 @@ const SectionTop = () => {
       <section className="app-info-section">
         <h1 className="app-info-title">Simple To Use</h1>
         <div className="mockup-container">
-          <div className="iphone-mockup">
+          <div className="iphone-mockup-top">
             {" "}
-            <div className="info-bubbles-container"></div>
-            <div className="info-bubble-right">
-              <p>Dashboard with quick insights and insipiring words.</p>
-            </div>
-            <div className="spacer"></div>
-            <div className="info-bubble-left">
-              <p>Journaling to promote and engage health tracking.</p>
-            </div>
-            <div className="spacer"></div>
-            <div className="info-bubble-right">
-              <p>Emoji faces to discover and learn about emotions.</p>
+            <div className="info-bubbles-container">
+              <div className="info-bubble-right">
+                <p>Dashboard with quick insights and insipiring words.</p>
+              </div>
+              <div className="spacer"></div>
+              <div className="info-bubble-left">
+                <p>Journaling to promote and engage health tracking.</p>
+              </div>
+              <div className="spacer"></div>
+              <div className="info-bubble-right">
+                <p>Emoji faces to discover and learn about emotions.</p>
+              </div>
             </div>
           </div>
         </div>
