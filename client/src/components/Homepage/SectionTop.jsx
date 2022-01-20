65397fb8b9dfe3f3741d62ec5d3a480db4b167c1
Stew Anoya <stewartanoya@gmail.com>
2022-01-20 15:30:40 -0500

    create info boxes to replace bubbles

--- client/src/components/Homepage/SectionTop.jsx
+++ client/src/components/Homepage/SectionTop.jsx
@@ -6,6 +6,34 @@ const SectionTop = () => {
         <div className="mockup-container">
           <div className="iphone-mockup-top">
             {" "}
+            <div className="info-section-list">
+              <div className="info-section-item">
+                <h2 className="info-card-title">
+                  Dashboard Resources and Affirmations
+                </h2>
+                <p className="info-card-body">
+                  The dashboard provides quick access to essential resources
+                  such as crisis support lines. The quotes are a great way to
+                  remind yourself of how important you are!
+                </p>
+              </div>
+              <div className="info-section-item">
+                <h2 className="info-card-title">Emoji Emotions</h2>
+                <p className="info-card-body">
+                  Emoji emotions is designed to teach kids about all the
+                  different emotions they can experience. It also provides tips
+                  on how to handle specific emotions.
+                </p>
+              </div>
+              <div className="info-section-item">
+                <h2 className="info-card-title">The Power of Journaling</h2>
+                <p className="info-card-body">
+                  Willow provides access to a journaling system with guided
+                  prompts. Journaling has been proven, time and time again, to
+                  be a useful mental health management tool.
+                </p>
+              </div>
+            </div>
             <div className="info-bubbles-container">
               <div className="info-bubble-right">
                 <p>Dashboard with quick insights and insipiring words.</p>
