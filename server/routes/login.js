b54f7d2bd6ed20f401351c2592bda8a27493b65c
Stew Anoya <stewartanoya@gmail.com>
2022-01-21 15:51:15 -0500

    add error handling and styling for error

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,26 +6,28 @@ const loginRouter = (db) => {
   router.post("/", function (req, res, next) {
     const userEmail = req.body.email;
 
+    // will check to see the type of login coming in and set the query accordingly
+    const queryString =
+      req.body.type === "student"
+        ? `SELECT * FROM students
+    WHERE email = $1;`
+        : `SELECT * FROM therapists
+    WHERE email = $1;`;
+
     console.log(req.body);
-
-    let queryString = "";
-
-    if (req.body.type === "student") {
-      queryString = `SELECT * FROM students
-      WHERE email = $1`;
-    } else {
-      queryString = `SELECT * FROM therapists
-      WHERE email = $1`;
-    }
-
+    console.log(queryString);
     const queryParams = [userEmail];
-    return db.query(queryString, queryParams).then((data) => {
-      if (req.body.password === data.rows[0].password) {
-        res.send({ id: data.rows[0].id, email: data.rows[0].email });
-      }
-    });
-
-    // console.log(users);
+    return db
+      .query(queryString, queryParams)
+      .then((data) => {
+        console.log(data.rows);
+        if (req.body.password === data.rows[0].password) {
+          res.send({ id: data.rows[0].id, email: data.rows[0].email });
+        } else {
+          res.send("invalid");
+        }
+      })
+      .catch((err) => res.send("invalid"));
   });
 
   return router;
