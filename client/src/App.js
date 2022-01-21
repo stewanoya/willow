9449768060915ad427d6d33e057942e18c686112
Muna Dahir <muna.dahir@hotmail.com>
2022-01-21 18:41:46 -0500

    add route for register

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Homepage/Home";
 import AuthForm from "./components/AuthForm";
 import Main from "./components/Main";
+import RegisterForm from "./components/form/RegisterForm";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -8,12 +9,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/user" element={<AuthForm />} />
-          <Route path="/main" element={<Main />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/user' element={<AuthForm />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/register' element={<RegisterForm />} />
         </Routes>
       </BrowserRouter>
     </div>
