2f4efbd2cc848575512d74e43d5691a9ee76b5dc
Stew Anoya <stewartanoya@gmail.com>
2022-01-21 10:06:15 -0500

    add logout confirm

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import willowLogo from "../Homepage/assets/willow-logo.svg";
+import ConfirmLogout from "./ConfirmLogout";
 
 import "./Sidebar.css";
 
 const Sidebar = (props) => {
   const navigate = useNavigate();
   const [selected, setSelected] = useState("home");
+  const [logout, setLogout] = useState(false);
 
   const sidebarNameClass = "sidebar-icon-name";
 
@@ -31,114 +33,130 @@ const Sidebar = (props) => {
   };
 
   const logoutHandler = () => {
+    setLogout(true);
+  };
+
+  const getConfirmLogoutChoice = (choice) => {
+    if (!choice) {
+      return setLogout(false);
+    }
+    confirmLogoutHandler();
+  };
+
+  const confirmLogoutHandler = () => {
     localStorage.removeItem("userID");
     localStorage.removeItem("user");
     navigate("/");
   };
 
   return (
-    <div className="sidebar-container">
-      <div className="sidebar-header">
-        <img
-          src={willowLogo}
-          alt="willow logo"
-          className="willow-logo-sidebar"
-        />
-      </div>
-      <ul className="sidebar-icons">
-        <div
-          className={`${sidebarNameClass} ${
-            selected === "home" ? "home-selected" : null
-          }`}
-          onClick={homeHandler}
-        >
-          {" "}
-          <li
+    <>
+      {logout && (
+        <ConfirmLogout getConfirmLogoutChoice={getConfirmLogoutChoice} />
+      )}
+      <div className="sidebar-container">
+        <div className="sidebar-header">
+          <img
+            src={willowLogo}
+            alt="willow logo"
+            className="willow-logo-sidebar"
+          />
+        </div>
+        <ul className="sidebar-icons">
+          <div
+            className={`${sidebarNameClass} ${
+              selected === "home" ? "home-selected" : null
+            }`}
             onClick={homeHandler}
-            className={`sidebar-icon-1 sidebar-item ${
-              selected === "home" ? "home-icon-selected" : null
-            }`}
           >
-            <img
-              className="icon-image"
-              alt="home button"
-              src={require("./sidebar_icons/home.png")}
-            />
-          </li>
-          <h1 className="sidebar-icon-label">Home</h1>
-        </div>
-        <div
-          className={`${sidebarNameClass} ${
-            selected === "journal" ? "journal-selected" : null
-          }`}
-          onClick={journalHandler}
-        >
-          <li
+            {" "}
+            <li
+              onClick={homeHandler}
+              className={`sidebar-icon-1 sidebar-item ${
+                selected === "home" ? "home-icon-selected" : null
+              }`}
+            >
+              <img
+                className="icon-image"
+                alt="home button"
+                src={require("./sidebar_icons/home.png")}
+              />
+            </li>
+            <h1 className="sidebar-icon-label">Home</h1>
+          </div>
+          <div
+            className={`${sidebarNameClass} ${
+              selected === "journal" ? "journal-selected" : null
+            }`}
             onClick={journalHandler}
-            className={`sidebar-icon-2 sidebar-item ${
-              selected === "journal" ? "journal-icon-selected" : null
-            }`}
           >
-            <img
-              className="icon-image"
-              alt="journals button"
-              src={require("./sidebar_icons/journal.png")}
-            />
-          </li>
-          <h1 className="sidebar-icon-label">Journals</h1>
-        </div>
-        <div
-          className={`${sidebarNameClass}  ${
-            selected === "calm" ? "calm-selected" : null
-          }`}
-          onClick={calmHandler}
-        >
-          <li
+            <li
+              onClick={journalHandler}
+              className={`sidebar-icon-2 sidebar-item ${
+                selected === "journal" ? "journal-icon-selected" : null
+              }`}
+            >
+              <img
+                className="icon-image"
+                alt="journals button"
+                src={require("./sidebar_icons/journal.png")}
+              />
+            </li>
+            <h1 className="sidebar-icon-label">Journals</h1>
+          </div>
+          <div
+            className={`${sidebarNameClass}  ${
+              selected === "calm" ? "calm-selected" : null
+            }`}
             onClick={calmHandler}
-            className={`sidebar-icon-3 sidebar-item ${
-              selected === "calm" ? "calm-icon-selected" : null
-            }`}
           >
-            <img
-              className="icon-image"
-              alt="calm corner button"
-              src={require("./sidebar_icons/calm.png")}
-            />
-          </li>
-          <h1 className="sidebar-icon-label">Calm Corner</h1>
-        </div>
-        <div
-          className={`${sidebarNameClass} ${
-            selected === "therapist" ? "therapist-selected" : null
-          }`}
-          onClick={therapistHandler}
-        >
-          <li
+            <li
+              onClick={calmHandler}
+              className={`sidebar-icon-3 sidebar-item ${
+                selected === "calm" ? "calm-icon-selected" : null
+              }`}
+            >
+              <img
+                className="icon-image"
+                alt="calm corner button"
+                src={require("./sidebar_icons/calm.png")}
+              />
+            </li>
+            <h1 className="sidebar-icon-label">Calm Corner</h1>
+          </div>
+          <div
+            className={`${sidebarNameClass} ${
+              selected === "therapist" ? "therapist-selected" : null
+            }`}
             onClick={therapistHandler}
-            className={`sidebar-icon-4 sidebar-item ${
-              selected === "therapist" ? "therapist-icon-selected" : null
-            }`}
           >
-            <img
-              className="icon-image"
-              alt="therapists button"
-              src={require("./sidebar_icons/therapist.png")}
-            />
-          </li>
-          <h1 className="sidebar-icon-label">Therapists</h1>
-        </div>
-        <div className={`${sidebarNameClass}`} onClick={logoutHandler}>
-          <li onClick={logoutHandler} className="sidebar-icon-5 sidebar-item">
-            <img
-              className="icon-image logout-button"
-              alt="logout button"
-              src={require("./sidebar_icons/logout.png")}
-            />
-          </li>
-          <h1 className="sidebar-icon-label">Logout</h1>
-        </div>
-      </ul>
-    </div>
+            <li
+              onClick={therapistHandler}
+              className={`sidebar-icon-4 sidebar-item ${
+                selected === "therapist" ? "therapist-icon-selected" : null
+              }`}
+            >
+              <img
+                className="icon-image"
+                alt="therapists button"
+                src={require("./sidebar_icons/therapist.png")}
+              />
+            </li>
+            <h1 className="sidebar-icon-label">Therapists</h1>
+          </div>
+          <div className={`${sidebarNameClass}`} onClick={logoutHandler}>
+            <li onClick={logoutHandler} className="sidebar-icon-5 sidebar-item">
+              <img
+                className="icon-image logout-button"
+                alt="logout button"
+                src={require("./sidebar_icons/logout.png")}
+              />
+            </li>
+            <h1 className="sidebar-icon-label">Logout</h1>
+          </div>
+        </ul>
+      </div>
+    </>
   );
 };
 
