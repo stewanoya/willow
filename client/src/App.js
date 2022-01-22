45e0c777ffcd5cad1603d2e744d64af74470ca81
Muna Dahir <muna.dahir@hotmail.com>
2022-01-22 13:18:48 -0500

    move themeProvider to App component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import Home from "./components/Homepage/Home";
 import AuthForm from "./components/AuthForm";
 import Main from "./components/Main";
 import RegisterForm from "./components/form/RegisterForm";
+import { ThemeProvider } from "@mui/material/styles";
+import Theme from "./components/ThemeMUI/Theme";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -9,16 +11,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/user' element={<AuthForm />} />
-          <Route path='/main' element={<Main />} />
-          <Route path='/register' element={<RegisterForm />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <div className='App'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/user' element={<AuthForm />} />
+            <Route path='/main' element={<Main />} />
+            <Route path='/register' element={<RegisterForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -4,8 +4,6 @@ import Dashboard from "./dashboard/Dashboard";
 import Sidebar from "./Sidebar/Sidebar";
 import Journal from "./Journal/Journal";
 import Therapist from "./Therapist/Therapist";
-import { ThemeProvider } from "@mui/material/styles";
-import Theme from "./ThemeMUI/Theme";
 
 import "./Main.css";
 const Main = () => {
@@ -17,15 +15,13 @@ const Main = () => {
     setView(view);
   };
   return (
-    <ThemeProvider theme={Theme}>
-      <div className='app-container'>
-        {view === "home" && <Dashboard />}
-        {view === "calm" && <CalmCorner />}
-        {view === "journal" && <Journal />}
-        {view === "therapist" && <Therapist />}
-        <Sidebar getView={getView} />
-      </div>
-    </ThemeProvider>
+    <div className='app-container'>
+      {view === "home" && <Dashboard />}
+      {view === "calm" && <CalmCorner />}
+      {view === "journal" && <Journal />}
+      {view === "therapist" && <Therapist />}
+      <Sidebar getView={getView} />
+    </div>
   );
 };
 
