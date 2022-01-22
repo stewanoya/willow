836cf0eecf0f4a34d99a3b616830d3a8fb7d2754
Muna Dahir <muna.dahir@hotmail.com>
2022-01-22 09:31:27 -0500

    move functions to form components

--- client/src/components/form/RegisterForm.jsx
+++ client/src/components/form/RegisterForm.jsx
@@ -9,105 +9,19 @@ import TherapistRegister from "./TherapistRegister";
 import axios from "axios";
 
 const RegisterForm = (props) => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState({});
   const [show, setShow] = useState(true);
 
-  const auth = async () => {
-    try {
-      const res = await axios.get("http://localhost:3002/login", {
-        auth: { username: user.email, password: user.password },
-      });
-      // if user does not exist add them to database and set localStorage
-      if (!res) {
-        console.log(res.data);
-
-        //make post to students / post to therapist
-        // axios.post("http://localhost:3002/login", student);
-        localStorage.setItem("user", res.data.email);
-        localStorage.setItem("userID", res.data.id);
-        navigate("/main");
-      }
-    } catch (e) {
-      console.log(e);
-    }
+  const close = () => {
+    setShow(false);
   };
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    auth();
-  };
-
-  const emailHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, email: e.target.value };
-    });
-  };
-
-  const passHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, password: e.target.value };
-    });
-  };
-
-  const nameHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, name: e.target.value };
-    });
-  };
-
-  const organizationHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, organization: e.target.value };
-    });
-  };
-
-  const titleHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, title: e.target.value };
-    });
-  };
-
-  const descriptionHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, description: e.target.value };
-    });
-  };
-
-  const profileHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, profile: e.target.value };
-    });
-  };
-
-  const phoneHandler = (e) => {
-    setUser((prev) => {
-      return { ...prev, phone: e.target.value };
-    });
-  };
-
   return (
     <div className='form-page'>
       <ThemeProvider theme={Theme}>
         <Nav setShow={setShow} />
         {show ? (
-          <StudentRegister
-            submitHandler={submitHandler}
-            emailHandler={emailHandler}
-            passHandler={passHandler}
-          />
+          <StudentRegister close={close} />
         ) : (
-          <TherapistRegister
-            submitHandler={submitHandler}
-            emailHandler={emailHandler}
-            passHandler={passHandler}
-            nameHandler={nameHandler}
-            organizationHandler={organizationHandler}
-            titleHandler={titleHandler}
-            descriptionHandler={descriptionHandler}
-            profileHandler={profileHandler}
-            phoneHandler={phoneHandler}
-          />
+          <TherapistRegister close={close} />
         )}
       </ThemeProvider>
     </div>
