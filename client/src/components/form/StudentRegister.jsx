4cd8ac0c35a04a21c8d72efeddf3ea16166cfb61
Muna Dahir <muna.dahir@hotmail.com>
2022-01-22 09:32:19 -0500

    add functions from the registerForm

--- client/src/components/form/StudentRegister.jsx
+++ client/src/components/form/StudentRegister.jsx
@@ -1,21 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 
+import axios from "axios";
+
 function StudentRegister(props) {
-  const { studentInfohandler } = props;
-  // const { studentInfohandler, emailHandler, passHandler } = props;
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+
+  const auth = async () => {
+    try {
+      const res = await axios.get("http://localhost:3002/login", {
+        auth: { username: user.email, password: user.password },
+      });
+      // if user does not exist add them to database and set localStorage
+      if (!res) {
+        console.log(res.data);
+        // axios.post("http://localhost:3002/login", student);
+        localStorage.setItem("user", res.data.email);
+        localStorage.setItem("userID", res.data.id);
+        navigate("/main");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    auth();
+  };
+
+  const emailHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, email: e.target.value };
+    });
+  };
+
+  const passHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, password: e.target.value };
+    });
+  };
 
   return (
     <div className='form-container'>
-      <form onSubmit={studentInfohandler} className='login-form'>
+      <form onSubmit={submitHandler} className='login-form'>
         <input
           placeholder='Email'
-          // onChange={emailHandler}
+          onChange={emailHandler}
           type='email'
           className='login-input'
         />
         <input
           placeholder='Password'
-          // onChange={passHandler}
+          onChange={passHandler}
           type='password'
           className='login-input'
         />
--- client/src/components/form/TherapistRegister.jsx
+++ client/src/components/form/TherapistRegister.jsx
@@ -1,21 +1,89 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 
+import axios from "axios";
+
 function TherapistRegister(props) {
-  const {
-    saveTherapist,
-    emailHandler,
-    passHandler,
-    nameHandler,
-    organizationHandler,
-    titleHandler,
-    descriptionHandler,
-    profileHandler,
-    phoneHandler,
-  } = props;
+  const navigate = useNavigate();
+  const [user, setUser] = useState({});
+
+  const auth = async () => {
+    try {
+      const res = await axios.get("http://localhost:3002/login", {
+        auth: { username: user.email, password: user.password },
+      });
+      // if user does not exist add them to database and set localStorage
+      if (!res) {
+        console.log(res.data);
+
+        //make post to students / post to therapist
+        // axios.post("http://localhost:3002/login", student);
+        localStorage.setItem("user", res.data.email);
+        localStorage.setItem("userID", res.data.id);
+        navigate("/main");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    auth();
+  };
+
+  const emailHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, email: e.target.value };
+    });
+  };
+
+  const passHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, password: e.target.value };
+    });
+  };
+
+  const nameHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, name: e.target.value };
+    });
+  };
+
+  const organizationHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, organization: e.target.value };
+    });
+  };
+
+  const titleHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, title: e.target.value };
+    });
+  };
+
+  const descriptionHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, description: e.target.value };
+    });
+  };
+
+  const profileHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, profile: e.target.value };
+    });
+  };
+
+  const phoneHandler = (e) => {
+    setUser((prev) => {
+      return { ...prev, phone: e.target.value };
+    });
+  };
 
   return (
     <div className='form-container'>
-      <form onSubmit={saveTherapist} className='login-form'>
+      <form onSubmit={submitHandler} className='login-form'>
         <input
           placeholder='Name'
           onChange={nameHandler}
