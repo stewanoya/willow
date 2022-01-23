0041ff99208ba140d4e4022b94ec58a4efa11424
Habon <habonhassan@Habons-MacBook-Air.local>
2022-01-23 16:10:09 -0500

    Fix JWT in journals

--- server/routes/journals.js
+++ server/routes/journals.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { authenticateToken } = require('../Helper/authenticate') 
+const { authenticateToken } = require("../Helper/authenticate");
 
 /* GET users listing. */
 const journalRouter = (db) => {
   router.get("/", authenticateToken, function (req, res, next) {
     // let studentEmail = req.params.email;
-    console.log("Here===>", req.user);
-
 
     // const studentEmail = req.signedCookies.name;
     const queryString = `SELECT journals.* FROM students
@@ -24,24 +22,44 @@ const journalRouter = (db) => {
   });
 
   router.post("/", authenticateToken, (req, res) => {
-    const userID = req.user.id;
-    const data = req.body;
+    const email = req.user.email;
+    const journal_data = req.body;
 
-    console.log("REQQQQ-->", data);
-    const queryString = `INSERT INTO journals (scale, title, entry, choice, student_id)
+    // console.log("STUDENT ID-->", student_id);
+
+    // const queryString = `INSERT INTO journals (scale, title, entry, choice, student_id)
+    // VALUES
+    // ($1, $2, $3, $4, $5);`;
+    // const queryParams = [
+    //   data.scale,
+    //   data.title,
+    //   data.description,
+    //   data.choice,
+    //   userID,
+    // ];
+    const queryString = `SELECT id FROM students WHERE students.email = $1;`;
+    const queryParams = [email];
+    return db
+      .query(queryString, queryParams)
+      .then((data) => {
+        const student_id = data.rows[0].id;
+        const queryString2 = `INSERT INTO journals (scale, title, entry, choice, student_id)
     VALUES
     ($1, $2, $3, $4, $5);`;
-    const queryParams = [
-      data.scale,
-      data.title,
-      data.description,
-      data.choice,
-      userID,
-    ];
-
-    return db.query(queryString, queryParams).catch((err) => {
-      console.log(err);
-    });
+        const queryParams2 = [
+          journal_data.scale,
+          journal_data.title,
+          journal_data.description,
+          journal_data.choice,
+          student_id,
+        ];
+        db.query(queryString2, queryParams2).catch((err) => {
+          console.log(err);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
   return router;
