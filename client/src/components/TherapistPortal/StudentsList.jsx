cf308a343a98f385c58361cc525271d2f206be5e
Muna Dahir <muna.dahir@hotmail.com>
2022-01-23 11:26:02 -0500

    add logic to studentList component

--- client/src/components/TherapistPortal/StudentsList.jsx
+++ client/src/components/TherapistPortal/StudentsList.jsx
@@ -1,3 +1,9 @@
+import StudentListItem from "./StudentListItem";
+import "./Student.css";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const StudentsList = () => {
   return <h1>students list</h1>;
 };
