33a3fb4af6b257ca71c0a66875aaf86170189fb1
Stew Anoya <stewartanoya@gmail.com>
2022-01-23 09:11:12 -0500

    make dashboard full page, add logout functionality

--- client/src/components/TherapistPortal/TherapistNav.jsx
+++ client/src/components/TherapistPortal/TherapistNav.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import ConfirmLogout from "../Sidebar/ConfirmLogout";
+import { useNavigate } from "react-router-dom";
+
 const TherapistNav = (props) => {
   const { getView } = props;
+
+  const [logout, setLogout] = useState(false);
+
+  const navigate = useNavigate();
 
   const profileHandler = () => {
     getView("profile");
@@ -10,23 +18,42 @@ const TherapistNav = (props) => {
   };
 
   const logoutHandler = () => {
-    console.log("this will logout eventually");
+    setLogout(true);
+  };
+
+  const getConfirmLogoutChoice = (choice) => {
+    if (!choice) {
+      return setLogout(false);
+    }
+    confirmLogoutHandler();
+  };
+
+  const confirmLogoutHandler = () => {
+    localStorage.removeItem("userID");
+    localStorage.removeItem("user");
+    localStorage.removeItem("userType");
+    navigate("/");
   };
 
   return (
-    <div className="therapist-nav-container">
-      <ul className="therapist-nav-list">
-        <li className="therapist-nav-item" onClick={profileHandler}>
-          Profile
-        </li>
-        <li className="therapist-nav-item" onClick={studentsHandler}>
-          Students
-        </li>
-        <li className="therapist-nav-item logout-nav" onClick={logoutHandler}>
-          Logout
-        </li>
-      </ul>
-    </div>
+    <>
+      {logout && (
+        <ConfirmLogout getConfirmLogoutChoice={getConfirmLogoutChoice} />
+      )}
+      <div className="therapist-nav-container">
+        <ul className="therapist-nav-list">
+          <li className="therapist-nav-item" onClick={profileHandler}>
+            Profile
+          </li>
+          <li className="therapist-nav-item" onClick={studentsHandler}>
+            Students
+          </li>
+          <li className="therapist-nav-item logout-nav" onClick={logoutHandler}>
+            Logout
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
