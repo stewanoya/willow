8f1a241e425849b3e1b587085a612a582dcaf0ca
Muna Dahir <muna.dahir@hotmail.com>
2022-01-24 13:42:45 -0500

    fix the route to remove therapist

--- server/routes/students.js
+++ server/routes/students.js
@@ -19,6 +19,20 @@ const studentsRouter = (db) => {
       .catch((err) => console.error(err));
   });
 
+  //get the id of the priamry therapist (if any)
+  router.get("/therapist", authenticateToken, function (req, res, next) {
+    const queryString = `SELECT therapist_id FROM students
+    WHERE id = $1;`;
+    // therapist id
+    const queryParams = [req.user.id];
+    return db
+      .query(queryString, queryParams)
+      .then((data) => {
+        res.json(data.rows);
+      })
+      .catch((err) => console.error(err));
+  });
+
   // set the primary therapist for a student
   router.post("/", authenticateToken, (req, res) => {
     const therapistId = req.params.id;
@@ -56,7 +70,7 @@ const studentsRouter = (db) => {
   //if the student removes a therapist then set the id to 0
   router.put("/remove_therapist", authenticateToken, function (req, res, next) {
     const userID = req.user.id;
-    const therapistId = 0;
+    const therapistId = req.body.data.id;
 
     const queryString = `UPDATE students
     SET therapist_id = $1
