866d8c3721aa4bd080244e6c141e113fb534af98
Muna Dahir <muna.dahir@hotmail.com>
2022-01-24 11:17:25 -0500

    render the full button conditionally and change the therapist availability

--- client/src/components/TherapistPortal/StudentsList.jsx
+++ client/src/components/TherapistPortal/StudentsList.jsx
@@ -1,6 +1,7 @@
 import StudentListItem from "./StudentListItem";
 import ShowJournalList from "./ShowJournalList";
 import { Button } from "@mui/material";
+import ConfirmFull from "./ConfirmFull";
 import "./Student.css";
 
 import { useEffect, useState } from "react";
@@ -10,6 +11,8 @@ const StudentsList = () => {
   const [show, setShow] = useState(false);
   const [selectedStudent, setSelectedStudent] = useState(null);
   const [students, setStudents] = useState([]);
+  const [full, setFull] = useState(false);
+  const [fullCapacity, setFullCapiacity] = useState(false);
 
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
@@ -44,22 +47,87 @@ const StudentsList = () => {
     );
   });
 
+  const fullHandler = () => {
+    setFull(true);
+  };
+
+  const getConfirmFullChoice = (choice) => {
+    if (!choice) {
+      return setFull(false);
+    }
+    confirmFullHandler();
+  };
+
+  const confirmFullHandler = () => {
+    // make axios request to do a post in therapist table
+    // set the full_capacity to true
+    const accessToken = localStorage.getItem("accessToken");
+    const data = { full: true };
+    axios
+      .put(
+        "http://localhost:3002/therapists/full",
+        {
+          data,
+        },
+        {
+          headers: { authorization: `Bearer ${accessToken}` },
+        }
+      )
+      .catch((err) => console.log(err));
+    setFullCapiacity(true);
+  };
+
+  const acceptHandler = () => {
+    // make axios request to do a post in therapist table
+    // set the full_capacity to true
+    const accessToken = localStorage.getItem("accessToken");
+    const data = { full: false };
+    axios
+      .put(
+        "http://localhost:3002/therapists/accept",
+        {
+          data,
+        },
+        {
+          headers: { authorization: `Bearer ${accessToken}` },
+        }
+      )
+      .catch((err) => console.log(err));
+    setFullCapiacity(false);
+  };
+
   return (
     <>
+      {full && <ConfirmFull getConfirmFullChoice={getConfirmFullChoice} />}
       {show ? (
         <ShowJournalList student={selectedStudent} exitShow={exitShow} />
       ) : (
-        <>
-          <Button
-            variant='contained'
-            color='primary'
-            type='submit'
-            className='full-button'
-          >
-            Full
-          </Button>
-          <div className='student-holder'>{parsedStudents}</div>
-        </>
+        [
+          <>
+            {fullCapacity ? (
+              <Button
+                variant='contained'
+                color='primary'
+                type='submit'
+                className='full-button'
+                onClick={acceptHandler}
+              >
+                Accept Students
+              </Button>
+            ) : (
+              <Button
+                variant='contained'
+                color='primary'
+                type='submit'
+                className='full-button'
+                onClick={fullHandler}
+              >
+                Full
+              </Button>
+            )}
+            <div className='student-holder'>{parsedStudents}</div>
+          </>,
+        ]
       )}
     </>
   );
