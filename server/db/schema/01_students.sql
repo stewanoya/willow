2a2f753cedc3638b8fc964be9c373372ef9fc51c
Muna Dahir <muna.dahir@hotmail.com>
2022-01-24 08:46:11 -0500

    add column to reference therapist

--- server/db/schema/01_students.sql
+++ server/db/schema/01_students.sql
@@ -8,6 +8,7 @@ CREATE TABLE students (
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   organization_name VARCHAR(255),
+  therapist_id INTEGER DEFAULT NULL REFERENCES therapists(id) ON DELETE CASCADE,
   username VARCHAR(255)
 );
 
