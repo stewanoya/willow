053412b49d2e97ac8a42d7e58eca527132baee6d
Muna Dahir <muna.dahir@hotmail.com>
2022-01-24 08:46:33 -0500

    add therapist id to seeds

--- server/db/seeds/01_students.sql
+++ server/db/seeds/01_students.sql
@@ -1,12 +1,12 @@
 -- Users seeds
-INSERT INTO students (email, password, organization_name, username) 
+INSERT INTO students (email, password, organization_name, therapist_id, username) 
 VALUES 
-('alice12@gmail.com', 'password123', 'Sunnyside Elementary School', 'Bear21'),
-('meow2021@gmail.com', 'pawsword123', 'Riverside Elementary School', 'Bird34'),
-('md23@gmail.com', 'password123', 'Spring Gardens School', 'Lion83'),
-('julia123@gmail.com', 'pawsword123', 'Elk Creek Middle School', 'King Cobra27'),
-('tina12@gmail.com', 'password123', 'Waterford Heights', 'Black Mamba24'),
-('studen2@gmail.com', 'password123', 'Waterford Heights', 'Horse99');
+('alice12@gmail.com', 'password123', 'Sunnyside Elementary School', '1','Bear21'),
+('meow2021@gmail.com', 'pawsword123', 'Riverside Elementary School','1', 'Bird34'),
+('md23@gmail.com', 'password123', 'Spring Gardens School', '1','Lion83'),
+('julia123@gmail.com', 'pawsword123', 'Elk Creek Middle School', '2',  'King Cobra27'),
+('tina12@gmail.com', 'password123', 'Waterford Heights', '1', 'Black Mamba24'),
+('studen2@gmail.com', 'password123', 'Waterford Heights', '2', 'Horse99');
 
 
 
