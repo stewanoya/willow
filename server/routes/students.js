83a19254f5a88085aa7650c983f3924e131123ef
Muna Dahir <muna.dahir@hotmail.com>
2022-01-24 13:30:48 -0500

    add queries to set and remove to primary therapist

--- server/routes/students.js
+++ server/routes/students.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { authenticateToken } = require("../Helper/authenticate");
 
 /* GET users listing. */
-const studentRouter = (db) => {
+const studentsRouter = (db) => {
   //get the list of students for specific that therapist
   router.get("/", authenticateToken, function (req, res, next) {
     const queryString = `SELECT students.* FROM students
@@ -36,6 +36,40 @@ const studentRouter = (db) => {
       });
   });
 
+  //set the primary therapist for the studenet ot the id of the therapist
+  router.put("/set_therapist", authenticateToken, function (req, res, next) {
+    const userID = req.user.id;
+    const therapistId = req.body.data.id;
+
+    const queryString = `UPDATE students
+    SET therapist_id = $1
+    WHERE id = $2;`;
+    const queryParams = [therapistId, userID];
+    return db
+      .query(queryString, queryParams)
+      .then((data) => {
+        res.send("saved");
+      })
+      .catch((err) => res.send("error"));
+  });
+
+  //if the student removes a therapist then set the id to 0
+  router.put("/remove_therapist", authenticateToken, function (req, res, next) {
+    const userID = req.user.id;
+    const therapistId = 0;
+
+    const queryString = `UPDATE students
+    SET therapist_id = $1
+    WHERE id = $2;`;
+    const queryParams = [therapistId, userID];
+    return db
+      .query(queryString, queryParams)
+      .then((data) => {
+        res.send("saved");
+      })
+      .catch((err) => res.send("error"));
+  });
+
   return router;
 };
-module.exports = studentRouter;
+module.exports = studentsRouter;
