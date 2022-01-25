56b96be18ff3f2f878f2b4af2b608fbc4f9538a4
Muna Dahir <muna.dahir@hotmail.com>
2022-01-24 20:42:04 -0500

    add query for therapist name, and change date default

--- server/routes/students.js
+++ server/routes/students.js
@@ -32,6 +32,20 @@ const studentsRouter = (db) => {
       })
       .catch((err) => console.error(err));
   });
+  //get the therapist info  of the priamry therapist (if any)
+  router.get("/therapist-info", authenticateToken, function (req, res, next) {
+    const queryString = `SELECT therapists.name, therapist_id FROM students
+    JOIN therapists ON therapists.id = students.therapist_id
+    WHERE students.id = $1;`;
+    // therapist id
+    const queryParams = [req.user.id];
+    return db
+      .query(queryString, queryParams)
+      .then((data) => {
+        res.json(data.rows);
+      })
+      .catch((err) => console.error(err));
+  });
 
   // set the primary therapist for a student
   router.post("/", authenticateToken, (req, res) => {
