edd6ba92f75995ef9096b9db0517c48eee7028db
Muna Dahir <muna.dahir@hotmail.com>
2022-01-24 20:40:22 -0500

    fix date, add therapist primary therapist header, add scale feelings

--- client/src/components/Journal/JournalListItem.jsx
+++ client/src/components/Journal/JournalListItem.jsx
@@ -6,6 +6,24 @@ const JournalListItem = (props) => {
   const clickHandler = () => {
     getJournal(id);
   };
+
+  const getFeeling = (scale) => {
+    if (scale === 1) {
+      return "Very bad";
+    }
+    if (scale === 2) {
+      return "Bad";
+    }
+    if (scale === 3) {
+      return "Meh";
+    }
+    if (scale === 4) {
+      return "Good";
+    } else {
+      return "Very good";
+    }
+  };
+
   return (
     <article onClick={clickHandler}>
       <section>
@@ -18,15 +36,17 @@ const JournalListItem = (props) => {
         </div>
         <div>
           <h4 className='journal-title-content'>{title}</h4>
+          <section className='journal-card-header'>
+            <p>{moment(date).format("dddd, MMMM Do YYYY")}</p>
+          </section>
         </div>
         <div className='journal-content'>
           <p className='journal-content-entry'>{entry}</p>
         </div>
         <section className='journal-card-footer'>
-          <p>{moment(date).startOf("minute").fromNow()}</p>
+          <p>Feeling: {getFeeling(scale)}</p>
           <p className='journal-arrow'>→</p>
         </section>
-        <p>{scale}</p>
       </section>
     </article>
   );
--- client/src/components/Therapist/Header.jsx
+++ client/src/components/Therapist/Header.jsx
@@ -1,19 +1,31 @@
-const Header = () => {
+const Header = (props) => {
+  const { therapistName } = props;
   return (
-    <div className='therapist-header'>
-      <article className='header-text'>
-        <h4>To book an appointment:</h4>
-        <p>
-          <span>1.</span> Choose the therpaist you'd like to book with.
-        </p>
-        <p>
-          <span>2.</span> Click the 'Book' button.
-        </p>
-        <p>
-          <span>3.</span> In the new window choose a day and time slot.
-        </p>
-      </article>
-    </div>
+    <>
+      {therapistName ? (
+        <div className='therapist-header'>
+          <article className='name-text'>
+            <h1>{therapistName}</h1>
+            <p> is your primary therapist </p>
+          </article>
+        </div>
+      ) : (
+        <div className='therapist-header'>
+          <article className='header-text'>
+            <h4>To book an appointment:</h4>
+            <p>
+              <span>1.</span> Choose the therpaist you'd like to book with.
+            </p>
+            <p>
+              <span>2.</span> Click the 'Book' button.
+            </p>
+            <p>
+              <span>3.</span> In the new window choose a day and time slot.
+            </p>
+          </article>
+        </div>
+      )}
+    </>
   );
 };
 
