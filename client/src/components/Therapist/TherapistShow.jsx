d61670a6dd502f8d07a54aa228a5875c36066b82
Muna Dahir <muna.dahir@hotmail.com>
2022-01-25 23:12:34 -0500

    add popup warning to set therapist

--- client/src/components/Therapist/TherapistShow.jsx
+++ client/src/components/Therapist/TherapistShow.jsx
@@ -1,3 +1,4 @@
+import ConfirmFull from "./ConfirmFull";
 import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import axios from "axios";
@@ -9,6 +10,7 @@ const TherapistShow = (props) => {
     props;
   const [open, setOpen] = useState(false);
   const [primary, setPrimary] = useState(false);
+  const [full, setFull] = useState(false);
 
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
@@ -33,7 +35,21 @@ const TherapistShow = (props) => {
     setPrimary(false);
   };
 
-  const setTherapistHandler = () => {
+  // const setTherapistHandler = () => {
+
+  //   setPrimaryTherapist(id);
+  //   setPrimary(true);
+  //   getTherapistData();
+  // };
+
+  const fullHandler = () => {
+    setFull(true);
+  };
+
+  const getConfirmFullChoice = (choice) => {
+    if (!choice) {
+      return setFull(false);
+    }
     setPrimaryTherapist(id);
     setPrimary(true);
     getTherapistData();
@@ -52,64 +68,78 @@ const TherapistShow = (props) => {
           />
         </div>
       ) : (
-        <div className='therapist-show'>
-          <div
-            className='close-button'
-            onClick={() => {
-              return props.exitShow();
-            }}
-          ></div>
-          <article className='therapist-details'>
-            <section className='therapist-details'>
-              <img
-                className='therapist-show-profile-pic'
-                src={img}
-                alt='profile'
+        [
+          <>
+            {full && (
+              <ConfirmFull
+                getConfirmFullChoice={getConfirmFullChoice}
+                name={name}
               />
-              <div className='therapist-title-content'>
-                <h3>{name}</h3>
-                <p className='therapist-info'>{description}</p>
-              </div>
-            </section>
-          </article>
-
-          <div className='therapist-detail-content '>
-            <h4>Organization:</h4>
-            <div className='therapist-details-personal'>
-              <p>{organization_name}</p>
-            </div>
-            <h4>Phone:</h4>
-            <div className='therapist-details-personal'>
-              <p>{phone}</p>
-            </div>
-            <h4>Email:</h4>
-            <div className='therapist-details-personal'>
-              <p>{email}</p>
-            </div>
-          </div>
-          <div className='therapist-show-buttons'>
-            <Button variant='contained' color='primary' onClick={clickHandler}>
-              <h4>Book Session</h4>
-            </Button>
-            {primary ? (
-              <Button
-                variant='contained'
-                color='secondary'
-                onClick={removeTherapistHandler}
-              >
-                <h4>Remove Therapist</h4>
-              </Button>
-            ) : (
-              <Button
-                variant='contained'
-                color='primary'
-                onClick={setTherapistHandler}
-              >
-                <h4>Set as Primary Therapist</h4>
-              </Button>
             )}
-          </div>
-        </div>
+            <div className='therapist-show'>
+              <div
+                className='close-button'
+                onClick={() => {
+                  return props.exitShow();
+                }}
+              ></div>
+              <article className='therapist-details'>
+                <section className='therapist-details'>
+                  <img
+                    className='therapist-show-profile-pic'
+                    src={img}
+                    alt='profile'
+                  />
+                  <div className='therapist-title-content'>
+                    <h3>{name}</h3>
+                    <p className='therapist-info'>{description}</p>
+                  </div>
+                </section>
+              </article>
+
+              <div className='therapist-detail-content '>
+                <h4>Organization:</h4>
+                <div className='therapist-details-personal'>
+                  <p>{organization_name}</p>
+                </div>
+                <h4>Phone:</h4>
+                <div className='therapist-details-personal'>
+                  <p>{phone}</p>
+                </div>
+                <h4>Email:</h4>
+                <div className='therapist-details-personal'>
+                  <p>{email}</p>
+                </div>
+              </div>
+              <div className='therapist-show-buttons'>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  onClick={clickHandler}
+                >
+                  <h4>Book Session</h4>
+                </Button>
+                {primary ? (
+                  <Button
+                    variant='contained'
+                    color='secondary'
+                    onClick={removeTherapistHandler}
+                  >
+                    <h4>Remove Therapist</h4>
+                  </Button>
+                ) : (
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    onClick={fullHandler}
+                  >
+                    <h4>Set as Primary Therapist</h4>
+                  </Button>
+                )}
+              </div>
+            </div>
+          </>,
+        ]
       )}
     </>
   );
